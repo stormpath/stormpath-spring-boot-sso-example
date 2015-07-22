d03b9ef6bc119e533b006919d824da52cfad0afb
Micah Silverman <micah@afitnerd.com>
2015-07-21 21:54:52 -0400

    Added in idSite. Added settings to ensure that redirects are ssl if that's how they started.

--- src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 
@@ -13,7 +15,9 @@ public class HomeController {
     Application app;
 
     @RequestMapping("/")
-    public String home(Model model) {
+    public String home(HttpServletRequest req, Model model) {
+        model.addAttribute("status", req.getParameter("status"));
+
         model.addAttribute("appName", app.getName());
         model.addAttribute("appDescription", app.getDescription());
 
